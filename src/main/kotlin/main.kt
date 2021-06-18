ed00106cfbfe432fd1e4271a3ed41d33556912b9
Dmitry Chuprikov <guide.baikal@gmail.com>
2021-06-18 16:43:35 +0800

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,29 @@
+import kotlin.math.roundToInt
+
+fun main() {
+    var cardType = "Maestro"
+    var sumOfTransactions = 80000.0
+    var currentTransaction = 1009.0
+
+    if(cardType=="Vk Pay" && currentTransaction<35) {
+        println("Сумма должна быть больше 35 руб")
+    }else if (cardType=="Vk Pay" && (currentTransaction>15000 || sumOfTransactions>40000)) {
+        print("вы превысили лимит по переводам")
+    }else if(currentTransaction>150000 || sumOfTransactions>600000){
+        print("вы превысили лимит по переводам")
+    }else{
+        var result = Math.round(transactions(cardType, sumOfTransactions, currentTransaction)*100.0)
+        println("Комиссия: $result коп")
+    }
+}
+
+fun transactions(cardType: String = "Vk Pay", sumOfTransactions: Double = 0.0, currentTransaction: Double): Double {
+    var commission = 0.0
+    when (cardType) {
+        "Mastercard", "Maestro" -> commission = if (sumOfTransactions > 300 && sumOfTransactions < 75000) 0.0 else (currentTransaction * 0.006 + 20)
+        "Visa", "Мир" -> commission = currentTransaction * 0.0075
+        "Vk Pay" -> commission =  0.0
+    }
+    return commission
+}
+
