335bfa564bc3737d9e5ee6165bdd9b16ed76bd7f
Dmitry Chuprikov <guide.baikal@gmail.com>
2021-06-23 09:45:32 +0800

    functions were improved

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,29 +1,45 @@
 import kotlin.math.roundToInt
 
 fun main() {
-    var cardType = "Maestro"
-    var sumOfTransactions = 80000.0 //в руб
-    var currentTransaction = 1009.0 //в руб
+    val cardType = "Mastercard"
+    val sumOfTransactions = 100000.0 //в руб
+    val currentTransaction = 1000.0 //в руб
 
-    if(cardType=="Vk Pay" && currentTransaction<35) {
-        println("Сумма должна быть больше 35 руб")
-    }else if (cardType=="Vk Pay" && (currentTransaction>15000 || sumOfTransactions>40000)) {
-        print("Вы превысили лимит по переводам")
-    }else if(currentTransaction>150000 || sumOfTransactions>600000){
-        print("Вы превысили лимит по переводам")
-    }else{
-        var result = Math.round(transactions(cardType, sumOfTransactions, currentTransaction)*100.0)
-        println("Комиссия: $result коп.") //в коп
-    }
+print(transactions(cardType, sumOfTransactions, currentTransaction))
+
 }
 
-fun transactions(cardType: String = "Vk Pay", sumOfTransactions: Double = 0.0, currentTransaction: Double): Double {
-    var commission = 0.0
+fun transactions(cardType: String = "Vk Pay",
+                 sumOfTransactions: Double = 0.0,
+                 currentTransaction: Double): Any =
     when (cardType) {
-        "Mastercard", "Maestro" -> commission = if (sumOfTransactions > 300 && sumOfTransactions < 75000) 0.0 else (currentTransaction * 0.006 + 20)
-        "Visa", "Мир" -> commission = currentTransaction * 0.0075
-        "Vk Pay" -> commission = 0.0
+        "Mastercard", "Maestro" -> commissionMastercard(sumOfTransactions, currentTransaction)
+        "Visa", "Мир" -> commissionVisa(currentTransaction, sumOfTransactions)
+        "Vk Pay" -> commissionVkPay(currentTransaction, sumOfTransactions)
+        else -> "Такая карта не принимается"
     }
-    return commission
-}
 
+fun commissionMastercard(sumOfTransactions: Double, currentTransaction: Double): Any =
+    when{
+        currentTransaction>150000 || sumOfTransactions>600000 -> "Вы превысили лимит по переводам"
+        sumOfTransactions > 300 && sumOfTransactions < 75000 -> printCommission(0.0)
+        else -> printCommission(currentTransaction * 0.006 + 20)
+    }
+
+fun commissionVisa(currentTransaction: Double, sumOfTransactions: Double): Any =
+    when {
+        currentTransaction>150000 || sumOfTransactions>600000 -> "Вы превысили лимит по переводам"
+        else -> printCommission(currentTransaction * 0.0075)
+    }
+
+fun commissionVkPay(currentTransaction: Double, sumOfTransactions: Double): String =
+    when {
+        currentTransaction<35 -> "Сумма должна быть больше 35 руб"
+        currentTransaction>15000 || sumOfTransactions>40000 -> "Вы превысили лимит по переводам"
+        else -> printCommission(currentTransaction * 0.0)
+    }
+
+fun printCommission(commission: Double): String{
+    val result = Math.round(commission*100.0)
+    return "Комиссия: $result коп."
+}
